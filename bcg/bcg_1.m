625d8a4219c7a3b3a947973a9dbc1508eb04c1d2
sclc <sclc@sclcs-MacBook-Pro.local>
2014-04-18 22:08:42 +0900

    commit bcg files

---
+++ bcg/bcg_1.m
@@ -0,0 +1,39 @@
+function [X,hist_residual] = bcg_1( A, RHS, X, maxIter, tol )
+% bcg_1 Summary of this function goes here
+% Detailed explanation goes here
+% A is sparse matrix
+% X, RHS are dense matrix
+% matIter are the max number of iteration 
+% tol is the breaking criteria, it will be compared to 
+%     relative residual |r|/|b|
+
+R = RHS - A * X;
+hist_residual(1,:)=[RelativeErrorCal(R,RHS,1,1)];
+fprintf ('first residual norm = %20.16f\n', hist_residual(1,1));
+Rtranspose_R=R' * R;
+P=R;
+for i = 1:maxIter
+    AP = A * P;
+    Ptranspose_AP = P' * AP;
+    ALPHA = Ptranspose_AP\Rtranspose_R;
+    X = X + P * ALPHA;
+    R = R - AP * ALPHA;
+    
+    Rtranspose_R_next = R' * R;
+    
+    hist_residual(i+1,:) = [RelativeErrorCal(R, RHS,1,1)];
+    if hist_residual(i+1,1) < tol
+        fprint ('final norm is %dth = %20.16f\n',i,hist_residual(i+1,1));
+        break;
+    end
+    
+    BETA = Rtranspose_R \ Rtranspose_R_next;
+    P = R + P * BETA;
+    
+    Rtranspose_R = Rtranspose_R_next;
+    fprintf ('%dth = %20.16f\n', i,hist_residual(i+1,1));
+end
+
+
+end
+
