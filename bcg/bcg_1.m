2cb3af90dc8c471611d4cae53fe175196b420311
scl <scl@ulp-at52.(none)>
2014-11-09 19:58:53 +0900

    sublime 3 sftp test

--- bcg/bcg_1.m
+++ bcg/bcg_1.m
@@ -6,34 +6,34 @@ function [X,hist_residual] = bcg_1( A, RHS, X, maxIter, tol )
 % matIter are the max number of iteration 
 % tol is the breaking criteria, it will be compared to 
 %     relative residual |r|/|b|
-RColInd = 1;
-RHSColInd = 1;
-R = RHS - A * X;
-hist_residual(1,:)=[RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
-fprintf ('first residual norm = %20.16f\n', hist_residual(1,1));
-Rtranspose_R=R' * R;
-P=R;
-for i = 1:maxIter
-    AP = A * P;
-    Ptranspose_AP = P' * AP;
-    ALPHA = Ptranspose_AP\Rtranspose_R;
-    X = X + P * ALPHA;
-    R = R - AP * ALPHA;
-    
-    Rtranspose_R_next = R' * R;
-    
-    hist_residual(i+1,:) = [RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
-    if hist_residual(i+1,1) < tol
-        fprintf ('final norm is %dth = %20.16f\n',i,hist_residual(i+1,1));
-        break;
+    RColInd = 1;
+    RHSColInd = 1;
+    R = RHS - A * X;
+    hist_residual(1,:)=[RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
+    fprintf ('first residual norm = %20.16f\n', hist_residual(1,1));
+    Rtranspose_R=R' * R;
+    P=R;
+    for i = 1:maxIter
+        AP = A * P;
+        Ptranspose_AP = P' * AP;
+        ALPHA = Ptranspose_AP\Rtranspose_R;
+        X = X + P * ALPHA;
+        R = R - AP * ALPHA;
+        
+        Rtranspose_R_next = R' * R;
+        
+        hist_residual(i+1,:) = [RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
+        if hist_residual(i+1,1) < tol
+            fprintf ('final norm is %dth = %20.16f\n',i,hist_residual(i+1,1));
+            break;
+        end
+        
+        BETA = Rtranspose_R \ Rtranspose_R_next;
+        P = R + P * BETA;
+        
+        Rtranspose_R = Rtranspose_R_next;
+        fprintf ('%dth = %20.16f\n', i,hist_residual(i+1,1));
     end
-    
-    BETA = Rtranspose_R \ Rtranspose_R_next;
-    P = R + P * BETA;
-    
-    Rtranspose_R = Rtranspose_R_next;
-    fprintf ('%dth = %20.16f\n', i,hist_residual(i+1,1));
-end
 
 
 end
--- cbcg/cbcg_1.m
+++ cbcg/cbcg_1.m
@@ -1,44 +1,44 @@
 function [x, hist_residual] = cbcg_1 (A, b, s_k, x, maxIters, tol)
 %% cbcg function 1st version
-[eigenValue_max, eigenValue_min] = gerschgorin_1(A);
-s_alpha = 2.0 / (eigenValue_max - eigenValue_min);
-s_beta  = - (eigenValue_max + eigenValue_min) / (eigenValue_max - eigenValue_min);
-r = b - A*x;
-hist_residual(1, :) = RelativeErrorCal(r, b , 1, 1);
-for ind = 1:maxIters
-    %% generate s_k dim subspace
-    s_k = uint32(s_k);
-    for dimInd = 1:s_k
-        if dimInd == 1
-            SS(:, 1) = r;
-        elseif dimInd == 2
-            SS (:, 2) = s_alpha * A * r + s_beta * r;
+    [eigenValue_max, eigenValue_min] = gerschgorin_1(A);
+    s_alpha = 2.0 / (eigenValue_max - eigenValue_min);
+    s_beta  = - (eigenValue_max + eigenValue_min) / (eigenValue_max - eigenValue_min);
+    r = b - A*x;
+    hist_residual(1, :) = RelativeErrorCal(r, b , 1, 1);
+    for ind = 1:maxIters
+        %% generate s_k dim subspace
+        s_k = uint32(s_k);
+        for dimInd = 1:s_k
+            if dimInd == 1
+                SS(:, 1) = r;
+            elseif dimInd == 2
+                SS (:, 2) = s_alpha * A * r + s_beta * r;
+            else
+                SS(:, dimInd) = 2 * s_alpha * A * SS(:, dimInd -1) ...
+                    + 2 * s_beta * SS(:,dimInd-1) - SS(:,dimInd-2);
+            end           
+        end
+        %% A-orthogonalization 
+        if ind == 1
+            Q = SS;
         else
-            SS(:, dimInd) = 2 * s_alpha * A * SS(:, dimInd -1) ...
-                + 2 * s_beta * SS(:,dimInd-1) - SS(:,dimInd-2);
-        end           
+            Qtran_A_SS = A_Q' * SS;
+            B = Qtran_A_Q_inv * Qtran_A_SS;
+            Q = SS - Q * B;
+        end
+        
+        %% get parameter to update x
+        A_Q = A * Q;
+        Qtran_A_Q = Q' * A_Q;
+        Qtran_A_Q_inv = inv(Qtran_A_Q);
+        para_alpha = Qtran_A_Q_inv * Q'* r;
+        
+        %% update x
+        x = x + Q * para_alpha;
+        r = r - A_Q * para_alpha;
+        hist_residual(ind+1, :) = RelativeErrorCal(r, b , 1, 1);
+        
+      
     end
-    %% A-orthogonalization 
-    if ind == 1
-        Q = SS;
-    else
-        Qtran_A_SS = A_Q' * SS;
-        B = Qtran_A_Q_inv * Qtran_A_SS;
-        Q = SS - Q * B;
-    end
-    
-    %% get parameter to update x
-    A_Q = A * Q;
-    Qtran_A_Q = Q' * A_Q;
-    Qtran_A_Q_inv = inv(Qtran_A_Q);
-    para_alpha = Qtran_A_Q_inv * Q'* r;
-    
-    %% update x
-    x = x + Q * para_alpha;
-    r = r - A_Q * para_alpha;
-    hist_residual(ind+1, :) = RelativeErrorCal(r, b , 1, 1);
-    
-  
-end
 
 end
