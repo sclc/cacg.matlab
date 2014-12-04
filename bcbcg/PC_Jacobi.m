71e1b6aaa89eb4731bbde401f15c83cef90c3f11
scl <scl@ulp-at52.(none)>
2014-12-05 00:06:35 +0900

    first version of Jacobi precondtioner

--- bcbcg/PC_Jacobi.m
+++ bcbcg/PC_Jacobi.m
@@ -1,3 +1,14 @@
-function [X,hist_residual] = PC_Jacobi(A, RHS, s_k, blocksize, X, maxIters, tol)
+function [A] = PC_Jacobi(A)
+% Jacobi precoditioner
+	[nrow,ncol] = size(A);
+	diagA = diag(A);
+	assert (nrow==ncol, 'this function need a square matrix as input');
+
+	for ind=1:nrow
+		assert (diaA(ind)~=0, 'this function cannot handle zero value element');
+		diaRoot = sqrt(diaA(ind));
+		A(ind,:) = A(ind,:) ./ diaRoot;
+		A(:,ind) = A(:,ind) ./ diaRoot;
+	end
 
 end
