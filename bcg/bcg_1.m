a3dddeddb2efdd208281f39c4e87ed525fcdadae
scl <scl@ulp-at52.(none)>
2015-04-23 18:15:45 +0900

    change hist info record format

--- bcg/bcg_1.m
+++ bcg/bcg_1.m
@@ -6,12 +6,13 @@ function [X,hist_residual] = bcg_1( A, RHS, X, maxIter, tol )
 % matIter are the max number of iteration 
 % tol is the breaking criteria, it will be compared to 
 %     relative residual |r|/|b|
-    RColInd = 1;
-    RHSColInd = 1;
+%    RColInd = 1;
+%    RHSColInd = 1;
     R = RHS - A * X;
-    hist_residual(1,:)=[RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
-    fprintf ('first residual norm = %20.16f\n', hist_residual(1,1));
-    Rtranspose_R=R' * R;
+%    hist_residual(1,:)=[RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
+    hist_residual(1, :) = [0,RelativeErrorCal(R, RHS , 1, 1)];
+    %fprintf ('first residual norm = %20.16f\n', hist_residual(1,1));
+    Rtranspose_R= R' * R;
     P=R;
     for i = 1:maxIter
         AP = A * P;
@@ -22,17 +23,18 @@ function [X,hist_residual] = bcg_1( A, RHS, X, maxIter, tol )
         
         Rtranspose_R_next = R' * R;
         
-        hist_residual(i+1,:) = [RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
-        if hist_residual(i+1,1) < tol
-            fprintf ('final norm is %dth = %20.16f\n',i,hist_residual(i+1,1));
-            break;
+%        hist_residual(i+1,:) = [RelativeErrorCal(R, RHS, RColInd, RHSColInd)];
+        hist_residual(i+1, :) = [i, RelativeErrorCal(R, RHS , 1, 1)];
+        if hist_residual(i+1,2) < tol
+        %    fprintf ('final norm is %dth = %20.16f\n',i,hist_residual(i+1,1));
+            return;
         end
         
         BETA = Rtranspose_R \ Rtranspose_R_next;
         P = R + P * BETA;
         
         Rtranspose_R = Rtranspose_R_next;
-        fprintf ('%dth = %20.16f\n', i,hist_residual(i+1,1));
+    %    fprintf ('%dth = %20.16f\n', i,hist_residual(i+1,1));
     end
 
 
